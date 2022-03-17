b010369371b849d65529d5da126a50433d285aee
InaiaKenia <inaiakenia15@gmail.com>
2022-03-16 20:54:46 -0600

    Rearrange divs and hide conteiners

---
+++ Assets/script.js
@@ -0,0 +1,32 @@
+// HTML Element Variables
+var startBtnEl = document.querySelector(".start-button");
+var quizContainer = document.getElementById("quiz-container");
+var results = document.getElementById("results");
+
+
+//Hide
+quizContainer.style.display = "none";
+results.style.display ="none";
+
+// Quiz Rules
+
+// Start Button
+
+// Start the time
+
+// Questions will be asked
+
+// Answer Options
+
+//Select one answer
+
+// Answer is Correct or Wrong
+
+// Add to the Score
+
+// Submit Quiz button 
+
+// Add Name , show score and save both.
+
+//  Ask to Start Over 
+
