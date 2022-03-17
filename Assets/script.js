ddde642952d8938e53ba4a763eb909ef6cb8b39a
InaiaKenia <inaiakenia15@gmail.com>
2022-03-16 21:15:46 -0600

    Added functions, arrays

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,83 @@
 var startBtnEl = document.querySelector(".start-button");
 var quizContainer = document.getElementById("quiz-container");
 var results = document.getElementById("results");
+var containerQuestions = document.querySelector(".container-questions");
+var option1 = document.querySelector("#op1");
+var option2 = document.querySelector("#op2");
+var option3 = document.querySelector("#op3");
+var option4 = document.querySelector("#op4");
 
-
+// Question
+var questionList =[
+    {
+        "q":"What is the extension of HTML file ?",
+        "option1":"hml",
+        "option2":"html",
+        "option3":"tml",
+        "option4":"css",
+         "answer":"html"
+    },
+    {
+        "q":"What is the extension of HTML file ?",
+        "option1":"hml",
+        "option2":"html",
+        "option3":"tml",
+        "option4":"css",
+         "answer":"html"
+    },
+    {
+        "q":"What is the extension of HTML file ?",
+        "option1":"hml",
+        "option2":"html",
+        "option3":"tml",
+        "option4":"css",
+         "answer":"html"
+    },
+    {
+        "q":"What is the extension of HTML file ?",
+        "option1":"hml",
+        "option2":"html",
+        "option3":"tml",
+        "option4":"css",
+         "answer":"html"
+    },
+    {
+        "q":"What is the extension of HTML file ?",
+        "option1":"hml",
+        "option2":"html",
+        "option3":"tml",
+        "option4":"css",
+         "answer":"html"
+    },
+    {
+        "q":"What is the extension of HTML file - last ?",
+        "option1":"hml",
+        "option2":"html",
+        "option3":"tml",
+        "option4":"css",
+         "answer":"html"
+    }
+]
+//
+var currentQ = 0;
 //Hide
 quizContainer.style.display = "none";
 results.style.display ="none";
 
+startBtnEl.addEventListener('click',function(){
+    quizContainer.style.display = "block";
+    startBtnEl.style.display = "none";
+    toDisplayQuestion()
+})
+
+
+function toDisplayQuestion(){
+     containerQuestions.textContent=questionList[currentQ].q;
+     option1.textContent = questionList[currentQ].option1
+     option2.textContent = questionList[currentQ].option2
+     option3.textContent = questionList[currentQ].option3
+     option4.textContent = questionList[currentQ].option4
+}
 // Quiz Rules
 
 // Start Button
